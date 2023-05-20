79248bdfb8abd65fdbbf691aba3a5e7cad695675
logotipza <87233733+logotipza@users.noreply.github.com>
2023-05-20 22:54:48 +0500

    2 calc + charts

--- js/loanCalculator.js
+++ js/loanCalculator.js
@@ -1,3 +1,5 @@
+var chart = null; // Переменная для хранения графика
+
 function togglePeriodType(type) {
   var radioMonths = document.getElementById("loanPeriodMonths");
   var radioYears = document.getElementById("loanPeriodYears");
@@ -25,13 +27,13 @@ function calculateLoan() {
   }
 
   var monthlyPayment = (loanAmount * loanInterest * Math.pow(1 + loanInterest, loanPeriod)) / (Math.pow(1 + loanInterest, loanPeriod) - 1);
-  document.getElementById("monthlyPayment").innerText = formatMoney(monthlyPayment) ;
+  document.getElementById("monthlyPayment").innerText = formatMoney(monthlyPayment);
 
   var totalLoanAmount = monthlyPayment * loanPeriod;
-  document.getElementById("totalLoanAmount").innerText = formatMoney(totalLoanAmount) ;
+  document.getElementById("totalLoanAmount").innerText = formatMoney(totalLoanAmount);
 
   var totalOverpayment = totalLoanAmount - loanAmount;
-  document.getElementById("totalOverpayment").innerText = formatMoney(totalOverpayment) ;
+  document.getElementById("totalOverpayment").innerText = formatMoney(totalOverpayment);
 
   var loanTable = document.getElementById("loanTable");
   // Очистка таблицы
@@ -39,18 +41,82 @@ function calculateLoan() {
     loanTable.deleteRow(1);
   }
   var loanBalance = loanAmount;
+  var principalPaymentData = []; // Массив для хранения данных платежей по основному долгу
   for (var i = 1; i <= loanPeriod; i++) {
     var interestPayment = loanBalance * loanInterest;
     var principalPayment = monthlyPayment - interestPayment;
     loanBalance -= principalPayment;
+    principalPaymentData.push(principalPayment); // Добавление значения платежа по основному долгу в массив
     var row = loanTable.insertRow(-1);
     var cell1 = row.insertCell(0);
     var cell2 = row.insertCell(1);
     var cell3 = row.insertCell(2);
     var cell4 = row.insertCell(3);
     cell1.innerText = i;
-    cell2.innerText = formatMoney(principalPayment) ;
-    cell3.innerText = formatMoney(interestPayment) ;
-    cell4.innerText = (loanBalance > 0 ? formatMoney(loanBalance) : "0.00") ;
+    cell2.innerText = formatMoney(principalPayment);
+    cell3.innerText = formatMoney(interestPayment);
+    cell4.innerText = (loanBalance > 0 ? formatMoney(loanBalance) : "0.00");
   }
+
+  // Удаление предыдущего графика, если он существует
+  if (chart) {
+    chart.destroy();
+  }
+
+  // Создание нового графика
+  var ctx = document.getElementById("chart").getContext("2d");
+
+  // Формирование данных для графика
+  var monthlyPaymentLine = Array.from({ length: loanPeriod }, () => monthlyPayment);
+  var principalPaymentLine = principalPaymentData;
+
+  chart = new Chart(ctx, {
+    type: "line",
+    data: {
+      labels: Array.from({ length: loanPeriod }, (_, i) => (i + 1).toString()),
+      datasets: [
+        {
+          label: "Ежемесячный платеж",
+          data: monthlyPaymentLine,
+          fill: false,
+          borderColor: "rgba(0, 0, 0, 1)",
+          borderWidth: 1,
+        },
+        {
+          label: "Платеж по основному долгу",
+          data: principalPaymentLine,
+          fill: false,
+          borderColor: "rgba(255, 0, 0, 1)",
+          borderWidth: 1,
+        },
+      ],
+    },
+    options: {
+      scales: {
+        x: {
+          display: true,
+          title: {
+            display: true,
+            text: "Месяц",
+          },
+        },
+        y: {
+          display: true,
+          title: {
+            display: true,
+            text: "Сумма, руб.",
+          },
+        },
+      },
+    },
+  });
+}
+
+function formatMoney(amount) {
+  return amount.toLocaleString("ru-RU", {
+    style: "currency",
+    currency: "RUB",
+    minimumFractionDigits: 2,
+    maximumFractionDigits: 2,
+  });
 }
--- js/numbers.js
+++ js/numbers.js
@@ -3,4 +3,20 @@ function formatMoney(amount) {
     var kopecks = Math.round((amount - rubles) * 100);
     return rubles.toLocaleString("ru-RU") + " руб. " + kopecks.toLocaleString("ru-RU", { minimumIntegerDigits: 2 }) + " коп.";
   }
+
+// Функция для форматирования числа с разделителем по разрядам
+// function formatNumber(number) {
+//   var parts = number.toString().split(".");
+//   parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, " ");
+//   return parts.join(".");
+// }
+
+// // Функция для преобразования числа в формат денег
+// function formatMoney(amount) {
+//   return formatNumber(amount.toFixed(2));
+// }
+
+
+
+
   
