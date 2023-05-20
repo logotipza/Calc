7f80071abdf9fe952c0355c3238d6f1e7d297ae2
logotipza <87233733+logotipza@users.noreply.github.com>
2023-05-20 23:33:33 +0500

    flex buttons

--- js/numbers.js
+++ js/numbers.js
@@ -4,18 +4,6 @@ function formatMoney(amount) {
     return rubles.toLocaleString("ru-RU") + " руб. " + kopecks.toLocaleString("ru-RU", { minimumIntegerDigits: 2 }) + " коп.";
   }
 
-// Функция для форматирования числа с разделителем по разрядам
-// function formatNumber(number) {
-//   var parts = number.toString().split(".");
-//   parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, " ");
-//   return parts.join(".");
-// }
-
-// // Функция для преобразования числа в формат денег
-// function formatMoney(amount) {
-//   return formatNumber(amount.toFixed(2));
-// }
-
 
 
 
